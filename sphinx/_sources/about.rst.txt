5fa373ef9190e755a118492f956fb971e4465c52
lefessan <fabrice.le_fessant@ocamlpro.com>
2022-08-19 16:39:39 +0000

    Deploying to gh-pages from  @ 81bf67ce9495180bb34fb16b6c22439f6984a25e 🚀

---
+++ sphinx/_sources/about.rst.txt
@@ -0,0 +1,11 @@
+About
+=====
+
+This is the description
+of the cargo-graph OCaml project
+
+
+Authors
+-------
+
+* Fabrice Le Fessant <fabrice.le_fessant@ocamlpro.com>
---
+++ sphinx/_static/documentation_options.js
@@ -0,0 +1,10 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: 'master (81bf67c) (2022/08/19 16:39)',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    FILE_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+};
